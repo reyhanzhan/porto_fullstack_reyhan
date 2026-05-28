5cfda92b676ae7c5489011c9ef34651480bc92c2
reyhan <reyhanrizqi01@gmail.com>
2026-05-28 14:56:24 +0700

    fix env vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig } from "@prisma/config";
 
+// Kita ambil langsung nilainya dari process.env agar validasi TypeScript aman
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    // Prisma 7 otomatis membaca DIRECT_URL dari env saat migrasi/db push dilakukan
+    url: process.env.DATABASE_URL,
   },
 });
